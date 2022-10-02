9109fe71318e2415a47cc284c6b0a93046490585
BakiTheEternal <bakieternal@gmx.com>
2022-10-02 15:13:22 -0400

    Cleared Repo
    Added a Gitpress Repo Clone to try and get the website working

---
+++ bin/docker_entrypoint.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -e
+# shellcheck disable=SC1091
+source gitpress_utils.sh
+
+mode="${GITPRESS_MODE:-FETCH}"
+
+if [ "$mode" != "SEED_VP" ] && [ "$mode" != "SEED_SQL" ] && [ "$mode" != "FETCH" ]; then
+  fatal "Mode ${mode} is not supported"
+fi
+
+initDB
+
+if [ "$mode" == "SEED_VP" ] || [ "$mode" == "SEED_SQL" ]; then
+  installWP
+
+  if [ "$mode" == "SEED_VP" ]; then
+    installVP
+  else
+    dumpDB
+    initGit
+  fi
+
+  setupGit
+
+  chown -R www-data .
+
+  pushGit
+else
+  if [[ ! -d ".git" ]]; then
+    echo "Git is not here will clone it"
+    cloneGit
+  fi
+
+  createWPConfig
+  createDB
+
+  restoreWP
+
+  chown -R www-data .
+fi
+
+service apache2 start
+
+if [[ ! -z "$SSL_DOMAIN" ]]; then
+  certbot --apache -d ${SSL_DOMAIN} --debug -n --agree-tos --email ${SSL_EMAIL}
+fi
+
+tail -f /dev/null
+
